2daf9ed64b73ef656f48ac3699c688953569e0de
Beatrix Devanti <beatrix.devanti.ft21@mail.umy.ac.id>
2022-12-30 17:58:14 +0700

    add method postorder

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace BinarySearchTree_001
 
         //Constructor for the node class
 
-        public Node( string i, Node l, Node r)
+        public Node(string i, Node l, Node r)
         {
             info = i;
             leftchild = l;
@@ -58,42 +58,118 @@ namespace BinarySearchTree_001
                 }
             }
         }
-    }
 
-    public void Search(string element,ref Node parent,ref Node currentNode)
-    {
-        /*This function searches the currentNode of the specified Node as 
-         well as the currentNode of its parent*/
-        currentNode = ROOT;
-        parent = null;
-        while((currentNode !=null) &&(currentNode.info !=element))
+        public void Search(string element, ref Node parent, ref Node currentNode)
         {
-            parent = currentNode;
-            if (string.Compare(element, currentNode.info) < 0)
-                currentNode = currentNode.leftchild;
-            else
-                currentNode = currentNode.rightchild;
+            /*This function searches the currentNode of the specified Node as 
+             well as the currentNode of its parent*/
+            currentNode = ROOT;
+            parent = null;
+            while ((currentNode != null) && (currentNode.info != element))
+            {
+                parent = currentNode;
+                if (string.Compare(element, currentNode.info) < 0)
+                    currentNode = currentNode.leftchild;
+                else
+                    currentNode = currentNode.rightchild;
+            }
         }
-    }
-    public void inorder(Node ptr)
-    {
-        if (ROOT == null)
+        public void inorder(Node ptr)
         {
-            Console.WriteLine("Tree is empty");
-            return;
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                inorder(ptr.leftchild);
+                Console.Write(ptr.info + " ");
+                inorder(ptr.rightchild);
+            }
         }
-        if (ptr != null)
+        public void preorder(Node ptr)
         {
-            inorder(ptr.leftchild);
-            Console.Write(ptr.info + " ");
-            inorder(ptr.rightchild);
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                preorder(ptr.leftchild);
+                preorder(ptr.rightchild);
+                Console.Write(ptr.info + "");
+            }
         }
-    }
-    public void preorder(Node ptr)
-    internal class Program
-    {
+
+        public void postorder(Node ptr)
+        {
+            if (ROOT ==null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            if (ptr != null)
+
+            {
+                postorder(ptr.leftchild);
+                postorder(ptr.rightchild);
+                Console.Write(ptr.info + " ");
+
+            }
+        }
+
         static void Main(string[] args)
+
         {
+            BinaryTree x = new BinaryTree();
+            while (true)
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. Implement insert operation");
+                Console.WriteLine("2.Perform inorder traversal");
+                Console.WriteLine("3.Perform inorder traversal");
+                Console.WriteLine("4.Perform postorder traversal");
+                Console.WriteLine("5.Exit");
+                Console.Write("\nEnter your choice (1-5) : ");
+                char ch = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine();
+                switch (ch)
+                {
+                    case '1':
+                        {
+                            Console.Write("Enter a word :");
+                            string word = Console.ReadLine();
+                            x.insert(word);
+                        }
+                        break;
+                    case '2':
+                        {
+                            x.inorder(x.ROOT);
+                        }
+                        break;
+                    case '3':
+                        {
+                            x.preorder(x.ROOT);
+                        }
+                        break;
+                    case '4':
+                        {
+                            x.postorder(x.ROOT);
+                          
+                        }
+                        break;
+                    case '5':
+                       return;
+                    default:
+                        {
+                            Console.WriteLine("Invalid option");
+                            break;
+                        }
+                }
+
+            }
         }
     }
 }
