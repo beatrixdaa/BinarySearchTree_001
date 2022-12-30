23c93afe88c32b5d07575a064904b712e450e8e3
Beatrix Devanti <beatrix.devanti.ft21@mail.umy.ac.id>
2022-12-30 17:14:11 +0700

    add class node

--- Program.cs
+++ Program.cs
@@ -6,5 +6,23 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree_001
 {
-    
+    class Node
+    {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+
+        //Constructor for the node class
+
+        public Node( string i, Node l, Node r)
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
 }
